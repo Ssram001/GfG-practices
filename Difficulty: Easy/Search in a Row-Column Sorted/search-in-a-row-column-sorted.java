b258bc7e2776fb77999fd0383edf09866d70092b
Sabyasachi <krram368@gmail.com>
2026-07-26 01:12:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in a Row-Column Sorted/search-in-a-row-column-sorted.java
+++ Difficulty: Easy/Search in a Row-Column Sorted/search-in-a-row-column-sorted.java
@@ -3,10 +3,19 @@ class Solution {
         // code here
         int n = arr.length, m = arr[0].length;
         
-        for (int i=0;i<n;i++){
-            for (int j=0;j<m;j++){
-                if(arr[i][j]==x) return true;
-            }
+        // for (int i=0;i<n;i++){
+        //     for (int j=0;j<m;j++){
+        //         if(arr[i][j]==x) return true;
+        //     }
+        // }
+        // return false;
+        
+        
+        int i=0, j=m-1; //T=O(M+N)
+        while(j>=0 && i<n){
+            if(arr[i][j]>x) j--;
+            else if (arr[i][j]<x) i++;
+            else return true;
         }
         return false;
     }
