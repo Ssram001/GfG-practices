2e28dac19146c28265c69c304b7c7e47b6b39657
Sabyasachi <krram368@gmail.com>
2026-07-31 15:12:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -1,10 +1,16 @@
 class Solution {
     int factorial(int n) {
         // code here
-          if(n==0 ) { // or n==0 || n==1 then return 1
-            return 1;
+        //   if(n==0 ) { // or n==0 || n==1 then return 1
+        //     return 1;
+        // }
+        // int fact= n*factorial(n-1);
+        // return fact;
+        
+        int fact=1;
+        for(int i =1;i<=n;i++){
+            fact*=i;
         }
-        int fact= n*factorial(n-1);
         return fact;
     }
 }
